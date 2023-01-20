db61665619c2ddf384892f4eab69e135731ef1ca
stsp93 <stspsp@gmail.com>
2023-01-20 19:17:22 +0200

    added navView

---
+++ src/views/navView.js
@@ -0,0 +1,30 @@
+import { render, html } from '../lib.js'
+import { getUser } from "../api/auth.js";
+
+const createNav = (user) => html`<button @click=${onNavClick} class="nav__button"><i class="fa-solid fa-bars"></i></button>
+<ul>
+
+
+    <li class="nav__li nav__hidden"><a href="/">Categories</a></li>
+    <!-- USER -->
+    ${user ? 
+    html`<li class="nav__li nav__hidden"><a href="/profile">Profile</a></li>
+    <li class="nav__li nav__hidden"><a href="/user/logout">Logout</a></li>`: 
+    html`<li class="nav__li nav__hidden"><a href="/user/login">Login</a></li>
+    <li class="nav__li nav__hidden"><a href="/user/register">Register</a></li>`}
+    <!-- GUEST -->
+
+</ul>`
+
+const nav = document.querySelector('.header__nav');
+
+export function showNav(ctx, next) {
+    const user = getUser()
+    render(createNav(user), nav)
+
+    next()
+}
+
+function onNavClick() {
+    document.querySelectorAll('.nav__li').forEach(el => el.classList.toggle('nav__hidden'));
+}
