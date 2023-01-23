3a55c626b4efe4dd912f88d7f935c1298962a4b3
stsp93 <stspsp@gmail.com>
2023-01-23 14:56:30 +0200

    added details/results view

---
+++ src/views/detailsView.js
@@ -0,0 +1,29 @@
+import { html } from "../lib.js";
+
+const detailsTemplate = () => html`<h2 class="main-title">&rAarr; <a href="">Clothing</a></h2>
+<article class="detials">
+    <img src="/static/images/clothings.jpg" alt="" class="main-image">
+    <div class="details-text">
+        <h3 class="details-title">
+            Kyoto VIP (skrilleaks Edit)
+        </h3>
+        <p class="details-date">01.03.2019</p>
+        <p class="price">Price: <strong>100</strong> $</p>
+        <p class="details-description"><strong>Description: </strong>  
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias voluptatem, deserunt perferendis, provident vitae recusandae, quasi tempore porro eligendi est temporibus laboriosam? Mollitia laboriosam blanditiis optio cumque architecto doloribus eos!
+        </p>
+        <br>
+        <p class="details-owner">Posted by: <em>john123</em></p>
+    </div>
+    <div class="details-buttons">
+        <a href="" class="details-edit">Edit</a>
+        <a href="" class="details-delete">Delete</a>
+        <a href="" class="details-contact">Contact</a>
+        <a href="" class="details-back">Back</a>
+    </div>
+
+</article>`
+
+export function showdetails(ctx, next) {
+    ctx.render(detailsTemplate())
+}
---
+++ src/views/resultsView.js
@@ -0,0 +1,80 @@
+import { html } from "../lib.js";
+
+const resultsTemplate = () => html`<h2 class="main-title">Offers</h2>
+<ul class="offers-list">
+  <li>
+    <article class="offer-card">
+      <a href="" class="offer-link"
+        ><img class="offer-img" src="/static/images/clothings.jpg" alt=""
+      /></a>
+
+      <div class="offer-text">
+        <a class="offer-title offer-link" href=""
+          >Brand new Nice Thingy that's red</a
+        >
+        <p class="location">Burgas, Bulgaria</p>
+        <p class="price"><strong>20.99</strong> $</p>
+      </div>
+    </article>
+  </li>
+
+  <li>
+      <article class="offer-card">
+        <a href="" class="offer-link"
+          ><img class="offer-img" src="/static/images/clothings.jpg" alt=""
+        /></a>
+
+        <div class="offer-text">
+          <a class="offer-title offer-link" href=""
+            >Brand new Nice Thingy that's red</a
+          >
+          <p class="location">Burgas, Bulgaria</p>
+          <p class="price"><strong>20.99</strong> $</p>
+        </div>
+      </article>
+    </li>
+
+    <li>
+      <article class="offer-card">
+        <a href="" class="offer-link"
+          ><img class="offer-img" src="/static/images/clothings.jpg" alt=""
+        /></a>
+
+        <div class="offer-text">
+          <a class="offer-title offer-link" href=""
+            >Brand new Nice Thingy that's red</a
+          >
+          <p class="location">Burgas, Bulgaria</p>
+          <p class="price"><strong>20.99</strong> $</p>
+        </div>
+      </article>
+    </li>
+
+    <li>
+      <article class="offer-card">
+        <a href="" class="offer-link"
+          ><img class="offer-img" src="/static/images/clothings.jpg" alt=""
+        /></a>
+
+        <div class="offer-text">
+          <a class="offer-title offer-link" href=""
+            >Brand new Nice Thingy that's red</a
+          >
+          <p class="location">Burgas, Bulgaria</p>
+          <p class="price"><strong>20.99</strong> $</p>
+        </div>
+      </article>
+    </li>
+</ul>
+
+<div class="pagination">
+  <button class="pagination-arrow"><i class="fa-solid fa-chevron-left"></i></button>
+  <p class="page">1/2</p>
+  <button class="pagination-arrow"><i class="fa-solid fa-chevron-right"></i></button>
+</div>
+<!--  if 0 offers  -->
+<!-- <p>No offers found...</p> -->`
+
+export function showResults(ctx, next) {
+    ctx.render(resultsTemplate())
+}
