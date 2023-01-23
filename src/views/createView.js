8a3540ddd456183ff39d5f37ac42177d7bccc7c8
stsp93 <stspsp@gmail.com>
2023-01-23 14:56:09 +0200

    added edit/create views

---
+++ src/views/createView.js
@@ -0,0 +1,61 @@
+import { html } from "../lib.js";
+
+const createTemplate = () => html`<h2 class="main-title">Create</h2>
+
+<form class="user-form" method="POST">
+  <article class="input-group">
+    <label for="title">Title*</label>
+    <input
+      id="title"
+      name="title"
+      type="text"
+      placeholder="Red Hat"
+    />
+  </article>
+  <article class="input-group">
+    <label for="category">Category*</label>
+    <select name="category" id="category">
+      <option value="Clothing">Clothing</option>
+      <option value="Electronics">Electronics</option>
+      <option value="Deals">Deals</option>
+      <option value="Entertainment">Entertainment</option>
+      <option value="Hobbies">Hobbies</option>
+      <option value="Housing">Housing</option>
+    </select>
+  </article>
+  <article class="input-group">
+      <label for="description">Description*</label>
+      <textarea name="description" id="description" cols="30" rows="5" placeholder="Describe the item"></textarea></article>
+  
+
+  <article class="input-group">
+    <label for="price">Price*</label>
+    <input
+      id="price"
+      type="text"
+      name="price"
+      placeholder="20.99"
+    />
+  </article>
+  <article class="input-group">
+    <label for="imageUrl">Image Url</label>
+    <input
+      id="imageUrl"
+      type="text"
+      name="imageUrl"
+      placeholder="http://example.com"
+    />
+  </article>
+  
+  <article class="input-group">
+      <p class="message-field">* Mandatory fields</p>
+  </article>
+
+  <article class="input-group">
+    <input class="action-button" type="submit" value="Create" />
+  </article>
+</form>`
+
+export function showCreate(ctx, next) {
+    ctx.render(createTemplate())
+}
--- src/views/registerView.js
+++ src/views/registerView.js
@@ -5,48 +5,28 @@ const registerTemplate = () => html`<h2 class="main-title">Register</h2>
 <form class="user-form" method="POST">
   <article class="input-group">
     <label for="email">Email*</label>
-    <input
-      id="email"
-      name="email"
-      type="email"
-      placeholder="john@abv.bg"
-    />
+    <input id="email" name="email" type="email" placeholder="john@abv.bg" />
   </article>
   <article class="input-group">
     <label for="username">Username*</label>
-    <input
-      id="username"
-      name="username"
-      type="text"
-      placeholder="john123"
-    />
+    <input id="username" name="username" type="text" placeholder="john123" />
   </article>
   <article class="input-group">
     <label for="password">Password*</label>
-    <input
-      id="password"
-      type="password"
-      name="password"
-      placeholder="********"
-    />
+    <input id="password" type="password" name="password" placeholder="********" />
   </article>
   <article class="input-group">
     <label for="repeatPassword">Repeat Password*</label>
-    <input
-      id="repeatPassword"
-      type="password"
-      name="repeatPassword"
-      placeholder="********"
-    />
+    <input id="repeatPassword" type="password" name="repeatPassword" placeholder="********" />
   </article>
   <article class="input-group">
     <p class="message-field">* Mandatory fields</p>
-</article>
+  </article>
   <article class="input-group">
-      <input class="action-button" type="submit" value="Register">
+    <input class="action-button" type="submit" value="Register">
   </article>
 </form>`
 
 export function showRegister(ctx, next) {
-    ctx.render(registerTemplate());
+  ctx.render(registerTemplate());
 }
